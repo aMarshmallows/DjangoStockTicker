a3d488e019015c77261da04d21f235c30d2da9cb
aMarshmallows <amasha667@gmail.com>
2022-03-12 11:24:02 -0500

    updated paths

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,5 +1,5 @@
 # every app needs its own urls.py file
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
@@ -7,13 +7,13 @@ from . import views
 # in django 2.2 we now use 'url' instead of 'path' to create paths
 # not sure what the r means but it works in stopping all urls matching the empty one
 urlpatterns = [
-    url('add_stock.html', views.add_stock, name="add_stock"),
+    path('add_stock.html', views.add_stock, name="add_stock"),
     # don't want delete.html - want to pass in stock id
     # not sure why (?P<stock_id>.*) works for allowing this url to take in data 
     # (simply using <stock_id> did not work) but it does!
-    url('delete/(?P<stock_id>.*)', views.delete, name="delete"),
-    url('delete_stock', views.delete_stock, name="delete_stock"),
-    url(r'^$', views.home, name="home"),
+    path('delete/(?P<stock_id>.*)', views.delete, name="delete"),
+    path('delete_stock', views.delete_stock, name="delete_stock"),
+    re_path(r'^$', views.home, name="home"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- stocks/urls.py
+++ stocks/urls.py
@@ -1,8 +1,8 @@
 
-from django.conf.urls import url, include
+from django.urls import re_path, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url('', include('quotes.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path('', include('quotes.urls')),
 ]
